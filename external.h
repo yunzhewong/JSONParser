093948fe92aae8d588157728b813a039f090e9c3
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-12-07 20:28:54 +1100

    found workflow for external compilation

---
+++ external.h
@@ -0,0 +1,6 @@
+#ifndef EXTERNAL_H
+#define EXTERNAL_H
+
+void randomFunction();
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+#include "external.h"
+
 int main() {
   std::cout << "Hello, from JSONParser!\n";
+  randomFunction();
   std::cout << "It is just a library for parsing JSONs\n";
 }
